c5887dd1c51271b7cbb1caf3624441be3bc9ec9d
ojisidavis <ojisidavis@gmail.com>
2025-01-01 09:35:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSidebar() {
+    const side = document.querySelector('.sidebar');
+    side.style.display = 'flex';
+}
+
+function hideSidebar() {
+    const side = document.querySelector('.sidebar');
+    side.style.display = 'none';
+}
